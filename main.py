e13d7608fa5d138c2f2c806b5156da01cb68ff2e
nilimpgp17046 <nilim.pgp17046@iimkashipur.ac.in>
2026-01-16 21:51:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ import pandas as pd
 import numpy as np
 import zipfile
 
-with zipfile.ZipFile("datasets.zip")
+with zipfile.ZipFile("datasets.zip") as z:
+  with z.open("walmart_data.csv") as f:
+    df = pd.read_csv(f)
+
+print(df)
+  
